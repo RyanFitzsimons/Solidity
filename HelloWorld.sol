4325e78f3a5706e4022fb541b6d04d1230e47d32
RyanFitzsimons <72200510+RyanFitzsimons@users.noreply.github.com>
2020-10-01 12:00:21 +0100

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.12;
+
+contract HelloWorld{
+    //STATE Variables
+    string public message = "Hello World Again";
+    
+    function getMessage() public view returns(string memory){
+        return message;
+    }
+    
+    function setMessage(string memory newMessage) public{
+        message = newMessage;
+    }
+    
+}
